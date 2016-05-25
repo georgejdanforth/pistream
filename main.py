43db9df2ab97a268778d69bff866fe52fe644374
George Danforth <georgejdanforth@gmail.com>
2016-05-25 17:23:09 -0400

    change execution order and thread control

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import os
 import time
 import subprocess
-from queue import Queue
-from threading import Thread, Lock
 
-from metadatareader import MetadataReader
 from shairportstreamreader import StreamReader
+from metadatareader import MetadataReader
 
 
-subProcArgs = ["shairport -M ~/pistream/metadata -l shairout -e shairout"]
+subProcArgs = ["shairport -b 100 -M ~/pistream/metadata -l shairout -e shairout"]
 metadataPath = "metadata/now_playing"
+
+
+def refresh_state():
+    for f in os.listdir("metadata"):
+        os.remove("metadata/" + f)
+    os.remove("shairout")
+    os.mkfifo("shairout")
 
 
 def main():
 
-    startTime = time.time()
-
-    metadataReaderLock = Lock()
-    metadataReaderQueue = Queue()
-    metadataReader = MetadataReader(metadataPath,
-                                    metadataReaderQueue,
-                                    metadataReaderLock)
+    refresh_state()
 
     shairport = subprocess.Popen(subProcArgs,
                                  stdout=None,
@@ -28,27 +27,36 @@ def main():
                                  shell=True,
                                  universal_newlines=True)
 
-    outReaderLock = Lock()
-    outReaderQueue = Queue()
-    outReader = StreamReader("shairout", outReaderQueue, outReaderLock)
+    threads = {"metadata":MetadataReader(metadataPath),
+               "stream":StreamReader("shairout")}
+    threads["stream"].start()
 
-    outReader.start()
-    metadataReader.start()
+    while not "now_playing" in os.listdir("metadata"):
+        time.sleep(1)
 
-    while time.time() - startTime < 45:
+    threads["metadata"].start()
+
+    startTime = time.time()
+
+    while time.time() - startTime < 45: # time limit for testing
+        art = [f for f in os.listdir("metadata") if f != "now_playing"]
+        if len(art) != 0:
+            for f in art:
+                os.remove("metadata/" + f)
         time.sleep(0.1)
 
-    with metadataReaderLock:
-        metadataReaderQueue.put('terminate')
-    with outReaderLock:
-        outReaderQueue.put('terminate')
-
-    while metadataReader.is_alive() or outReader.is_alive:
-        pass
-    metadataReader.join()
-    outReader.join()
-
     shairport.terminate()
+
+    for t in threads.values():
+        t.stop()
+
+    while any([threads[t].is_alive() for t in threads.keys()]):
+        print([t for t in threads.keys() if threads[t].is_alive()])
+        time.sleep(1)
+
+    for t in threads.values():
+        t.join()
+
 
 
 if __name__ == '__main__':
